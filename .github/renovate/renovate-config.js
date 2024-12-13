07a86b37996ac478933a8bb460874258949ae710
Francis Fortier <francis.fortier@ticketmaster.com>
2024-12-13 09:25:19 -0500

    build: more renovate config

--- .github/renovate/renovate-config.js
+++ .github/renovate/renovate-config.js
@@ -4,7 +4,5 @@ module.exports = {
         "^\\.\\/\.github\\/renovate\\/post-upgrade\\.sh"
     ],
     platform: 'github',
-    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     repositories: ['ffortier/mono'],
-    username: 'renovate',
 }
