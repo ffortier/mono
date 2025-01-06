8f86a2f049c515aa6c6a2acf9cfd2aa240d82d5b
Francis Fortier <francis.fortier@gmail.com>
2025-01-06 08:21:51 -0500

    build: update renovate config for pip

--- .github/renovate/post-upgrade.sh
+++ .github/renovate/post-upgrade.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/env bash
 
+if ! git diff --exit-code third_party/python/requirements.in >/dev/null; then
+    bazel run //third_party/python:requirements.update
+fi
+
 REPIN=1 bazel build --nobuild //...
+
