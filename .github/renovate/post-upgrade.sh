73e72a7c4f8dcfbf880212f2b706b2e6386c979c
Francis Fortier <francis.fortier@gmail.com>
2025-01-06 08:21:53 -0500

    docs: mkdocs monorepo plugin

--- .github/renovate/post-upgrade.sh
+++ .github/renovate/post-upgrade.sh
@@ -2,6 +2,7 @@
 
 if ! git diff --exit-code third_party/python/requirements.in >/dev/null; then
     bazel run //third_party/python:requirements.update
+    bazel run //third_party/python:requirements_3_9.update
 fi
 
 REPIN=1 bazel build --nobuild //...
