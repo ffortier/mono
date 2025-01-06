85e07aebdca65236e0be032681680dea453cb3c6
Francis Fortier <francis.fortier@gmail.com>
2025-01-06 08:21:51 -0500

    build: change renovate config

--- .github/renovate/renovate-config.js
+++ .github/renovate/renovate-config.js
@@ -2,5 +2,9 @@ module.exports = {
     allowPostUpgradeCommandTemplating: true,
     allowedPostUpgradeCommands: [
         /^\.\/\.github\/renovate\/post-upgrade\.sh/
-    ]
+    ],
+    platform: 'github',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+    repositories: ['ffortier/mono'],
+    username: 'renovate',
 }
