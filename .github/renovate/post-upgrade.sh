924946f7a7c8040e0bc00adc6eaf60fb1c59f6df
ffortier <francis.fortier@gmail.com>
2025-02-22 07:31:14 -0500

    Deps: repin maven after renovate

--- .github/renovate/post-upgrade.sh
+++ .github/renovate/post-upgrade.sh
@@ -5,5 +5,9 @@ if ! git diff --exit-code third_party/python/requirements.in >/dev/null; then
     bazel run //third_party/python:requirements_3_9.update
 fi
 
+if ! git diff --exit-code third_party/java/deps.MODULE.bazel >/dev/null; then
+  REPIN=1 bazel run @maven//:pin
+fi
+
 REPIN=1 bazel build --nobuild //...
 
