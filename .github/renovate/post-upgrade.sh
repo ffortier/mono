8c3239ef84997847b6e3fada6c6d11b075b56236
ffortier <francis.fortier@gmail.com>
2025-02-28 21:55:27 -0500

    Update post-upgrade.sh

--- .github/renovate/post-upgrade.sh
+++ .github/renovate/post-upgrade.sh
@@ -6,7 +6,11 @@ if ! git diff --exit-code third_party/python/requirements.in >/dev/null; then
 fi
 
 if ! git diff --exit-code third_party/java/deps.MODULE.bazel >/dev/null; then
-  REPIN=1 bazel run @maven//:pin
+    REPIN=1 bazel run @maven//:pin
+fi
+
+if ! git diff --exit-code third_party/dotnet/paket.dependencies >/dev/null; then
+    bazel run //third_party/dotnet:update-deps
 fi
 
 REPIN=1 bazel build --nobuild //...
