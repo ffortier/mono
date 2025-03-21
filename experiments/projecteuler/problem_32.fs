c2576ec1b896df7ddbdfdb80499690c870f3f0dc
Francis Fortier <francis.fortier@gmail.com>
2025-03-21 12:11:01 -0400

    euler: problem32

---
+++ experiments/projecteuler/problem_32.fs
@@ -0,0 +1,46 @@
+open System
+
+let insert_with_permutations (n: int) (arr: int array) = seq {
+    for i in 0..Array.length arr do
+        yield Array.insertAt i n arr
+}
+
+let rec pandigital (n: int) (digits: int array) = 
+    match n with
+    | 0 -> seq {digits}
+    | _ -> insert_with_permutations n digits
+           |> Seq.map (pandigital (n - 1)) 
+           |> Seq.concat
+
+let make_num (digits: int array) =
+    let rec make_num_ (digits: int list) (n: int) =
+        match digits with
+        | [] -> n
+        | d :: digits' -> make_num_ digits' (n * 10 + d)
+
+    make_num_ (Array.toList digits) 0
+
+let group_numbers (digits: int array) = seq {
+    let n = Array.length digits
+    let first_n = int (Math.Floor ((decimal n) / 3.0m))
+
+    for f in 0..first_n - 1 do
+        let second_n = int (Math.Floor (((decimal n) - (decimal f)) / 2.0m))
+        for s in f + 1..f + second_n do
+            let a = digits[0..f] |> make_num
+            let b = digits[f+1..s] |> make_num
+            let c = digits[s+1..] |> make_num
+
+            yield (a, b, c)
+}
+
+pandigital 9 [| |]
+|> Seq.map group_numbers
+|> Seq.concat
+|> Seq.filter (fun (a, b, c) -> a < b && b < c)
+|> Seq.filter (fun (a, b, c) -> a * b = c)
+|> Seq.map (fun (_, _, c) -> c)
+|> Seq.sort
+|> Seq.distinct
+|> Seq.sum
+|> printfn "%A"
