ccd2de1c42fb3805678c95f2435037907fac4764
ffortier <francis.fortier@gmail.com>
2025-03-30 16:23:09 -0400

    Update post-upgrade.sh

--- .github/renovate/post-upgrade.sh
+++ .github/renovate/post-upgrade.sh
@@ -3,7 +3,6 @@ set -xe
 
 if ! git diff --exit-code third_party/python/requirements.in >/dev/null; then
     bazel run //third_party/python:requirements.update
-    bazel run //third_party/python:requirements_3_9.update
 fi
 
 if ! git diff --exit-code third_party/java/deps.MODULE.bazel >/dev/null; then
