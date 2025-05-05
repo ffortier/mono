587cbe5027f9797647ebdba399708b920069945e
ffortier <francis.fortier@gmail.com>
2025-05-05 07:36:53 -0400

    Update renovate-config.js

--- .github/renovate/renovate-config.js
+++ .github/renovate/renovate-config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    allowCommandTemplating: true,
-    allowedCommands: ["^\\.\\/\.github\\/renovate\\/post-upgrade\\.sh"],
-    platform: 'github',
-    repositories: ['ffortier/mono'],
+    "allowedCommands": ["^\\.\\/.github\\/renovate\\/post-upgrade\\.sh"],
+    "platform": "github",
+    "repositories": ["ffortier/mono"],
 }
