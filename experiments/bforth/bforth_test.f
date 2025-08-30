f9a300a35ec7b8bfc14085190aa82dbad844a59d
Francis Fortier <francis.fortier@gmail.com>
2025-08-30 10:06:09 -0400

    bforth: fix output and kill jobs on exit

--- experiments/bforth/bforth_test.f
+++ experiments/bforth/bforth_test.f
@@ -1,17 +1,17 @@
 ( Unit tests )
 
-: asserteq over over = 0= if ." Assertion failed, values are not equal" . . 1 exit else drop drop then ;
-: fasserteq over over = 0= if ." Assertion failed, values are not equal" f. f. 1 exit else drop drop then ;
-: ASSERTEMPTY depth 0 > if ." Assertion failed, stack is not empty" .s 1 exit then ;
+: asserteq over over = 0= if ." Assertion failed, values are not equal: " . ." !=" . cr 1 exit else drop drop then ;
+: fasserteq over over = 0= if ." Assertion failed, values are not equal: " f. ." !=" f. cr 1 exit else drop drop then ;
+: ASSERTEMPTY depth 0 > if ." Assertion failed, stack is not empty: " .s 1 cr exit then ;
 
-." test arithmetic"
+." test arithmetic" cr
 2 3 * 6 asserteq
 2 3 + 5 asserteq
 2 3 - -1 asserteq
 6 3 / 2 asserteq
 assertempty
 
-." test allot"
+." test allot" cr
 HERE 0 asserteq
 5 ALLOT
 here 5 asserteq
@@ -19,12 +19,12 @@ here 5 asserteq
 here 2 asserteq
 assertempty
 
-." test constant"
+." test constant" cr
 123 constant x123
 x123 123 ASSERTEQ
 assertempty
 
-." test variable"
+." test variable" cr
 variable v1
 variable v2
 variable v3
@@ -34,15 +34,15 @@ variable v3
 v1 @ v2 @ v3 @ + + 2334 asserteq 
 assertempty
 
-." test DO ... LOOP"
+." test DO ... LOOP" cr
 0 10 0 do 2 + loop 20 asserteq
 assertempty
 
-." test BEGIN ... UNTIL"
+." test BEGIN ... UNTIL" cr
 10 begin 1 - dup 0= until 0 asserteq
 assertempty
 
-." test fixed-point"
+." test fixed-point" cr
 1.234 1234000 asserteq
 .234 234000 asserteq
 1. 1000000 asserteq
@@ -54,4 +54,3 @@ assertempty
 10. 3. f/ 3.333333 fasserteq
 10. 8. f/ 1.25 fasserteq
 assertempty
-
