ff108f15f5187b8579ca626667531668afb5b647
Francis Fortier <francis.fortier@ticketmaster.com>
2026-01-02 21:40:54 -0500

    minimal forth interpreter

--- experiments/c64th/lexer.c
+++ experiments/c64th/lexer.c
@@ -1,5 +1,7 @@
 #include "lexer.h"
 
+#include <string.h>
+
 void lexer_init(Lexer* lexer, CharStream* stream) {
   lexer->stream = stream;
   lexer->current_token.kind = TOKEN_EOF;
@@ -7,35 +9,57 @@ void lexer_init(Lexer* lexer, CharStream* stream) {
 
 Token* lexer_get_token(Lexer* lexer) { return &lexer->current_token; }
 
-TokenKind lexer_next(Lexer* lexer) {
+static int read_number(Lexer* lexer, char first_char) {
+  int number = first_char - '0';
+  char c;
+
   while (char_stream_next(lexer->stream)) {
-    char c = char_stream_get_char(lexer->stream);
+    c = char_stream_get_char(lexer->stream);
     if (c >= '0' && c <= '9') {
-      int number = 0;
-      do {
-        number = number * 10 + (c - '0');
-        if (!char_stream_next(lexer->stream)) {
-          break;
-        }
-        c = char_stream_get_char(lexer->stream);
-      } while (c >= '0' && c <= '9');
-      lexer->current_token.kind = TOKEN_NUMBER;
-      lexer->current_token.as.number = number;
-      return TOKEN_NUMBER;
-    } else if (c == '+') {
-      lexer->current_token.kind = TOKEN_PLUS;
-      return TOKEN_PLUS;
-    } else if (c == '-') {
-      lexer->current_token.kind = TOKEN_MINUS;
-      return TOKEN_MINUS;
-    } else if (c == '*') {
-      lexer->current_token.kind = TOKEN_ASTERISK;
-      return TOKEN_ASTERISK;
-    } else if (c == '/') {
-      lexer->current_token.kind = TOKEN_SLASH;
-      return TOKEN_SLASH;
+      number = number * 10 + (c - '0');
+    } else {
+      return number;
     }
   }
-  lexer->current_token.kind = TOKEN_EOF;
-  return TOKEN_EOF;
+
+  return number;
+}
+
+TokenKind lexer_next(Lexer* lexer) {
+  memset(&lexer->current_token, 0, sizeof(Token));
+
+  while (char_stream_next(lexer->stream)) {
+    char c = char_stream_get_char(lexer->stream);
+
+    switch (c) {
+      case '+':
+        lexer->current_token.kind = TOKEN_PLUS;
+        goto done;
+      case '-':
+        lexer->current_token.kind = TOKEN_MINUS;
+        goto done;
+      case '*':
+        lexer->current_token.kind = TOKEN_ASTERISK;
+        goto done;
+      case '/':
+        lexer->current_token.kind = TOKEN_SLASH;
+        goto done;
+      case '.':
+        lexer->current_token.kind = TOKEN_DOT;
+        goto done;
+      default:
+        if (c >= '0' && c <= '9') {
+          lexer->current_token.kind = TOKEN_NUMBER;
+          lexer->current_token.as.number = read_number(lexer, c);
+          goto done;
+        }
+
+        // Ignore unknown characters
+
+        break;
+    }
+  }
+done:
+
+  return lexer->current_token.kind;
 }
