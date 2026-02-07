675786ccd9d3b531db0c7f55107be7b00865ab37
Francis Fortier <francis.fortier@gmail.com>
2026-02-07 09:22:35 -0500

    bitrot: state machine experiment

--- experiments/bitrot/bitrot.c
+++ experiments/bitrot/bitrot.c
@@ -3,33 +3,57 @@
 #include <conio.h>
 #include <peekpoke.h>
 #include <stdint.h>
+#include <stdio.h>
 
 typedef struct {
   char* text;
   uint8_t pos;
-  uint8_t offset;
+  int offset;
 } text_t;
 
 text_t text = {0};
 
-#define VIDEO_MEM(offset) *((char*)(0x0400 + offset))
+typedef void (*state_handler)(void);
+
+#define VIDEO_MEM(offset) *((unsigned char*)(0x0400 + offset))
+#define STATE (*(state_handler*)0x40)
+
+void first(void);
+void second(void);
+
+void dispatch(void) {
+  asm("jmp ($40)");
+  printf("Unreacheable!\n");
+}
 
 int main(void) {
+  int i;
   //  POKE(53272, 21);  // Enable uppercase + graphics mode
+
+  STATE = &first;
 
   clrscr();
 
-  text.text = "hello world";
+  text.text = "hellorld";
 
   while (1) {
     waitvsync();
-    waitvsync();
-    waitvsync();
-    waitvsync();
-    waitvsync();
-    waitvsync();
-    waitvsync();
-
-    if (text.text[text.pos]) VIDEO_MEM(text.offset++) = text.text[text.pos++];
+    dispatch();
   }
+}
+
+void first(void) {
+  if (text.text[text.pos]) {
+    VIDEO_MEM(text.offset++) = text.text[text.pos++];
+  } else {
+    STATE = &second;
+  }
+}
+
+void second(void) {
+  text.pos = 0;
+  while (text.offset % 40) text.offset++;
+
+  text.text = "yup";
+  STATE = &first;
 }
